cf0076c77d588f0428dc7972455f37f58c915ec6
NicoDVKa <herlannicolas6@gmail.com>
2024-04-05 23:04:10 -0300

    add carousel

--- src/Pages/DetailCountry.jsx
+++ src/Pages/DetailCountry.jsx
@@ -3,107 +3,120 @@ import { useQuery } from "@tanstack/react-query";
 import { getCountryByCode } from "../Services/CountryService";
 import CircularProgress from "@mui/material/CircularProgress";
 import "./DetailCountry.css";
+import Carousel from 'react-material-ui-carousel'
 
 const DetailCountry = () => {
   let [code] = useSearchParams();
   code = code.get("code");
-
+  
   const { isLoading, data, isError, error } = useQuery({
     queryKey: [code],
     queryFn: () => getCountryByCode(code),
   });
 
-  if (isLoading)
-    return (
-      <CircularProgress
-        style={{ alignSelf: "center", width: "75px", height: "75px" }}
-      />
-    );
+  if (isLoading) return  <CircularProgress style={{ alignSelf: "center", width: "75px", height: "75px" }}/>
   else if (isError) return <div>Error: {error.message}</div>;
-  console.log(data);
+
   const country = data;
   let name = country.name;
   let coatOfArms = country.coatOfArms?.png;
-  let languages = country.languages
+  let languages = country.languages;
+  let borders = country.borders;
 
   return (
     <>
-    <div className="country-detail">
-      <section className="country">
-        <div
-          className={`country__details country__name ${
-            !coatOfArms ? "country__name--extended" : ""
-          }`}
-        >
-          <h1 className="">{name}</h1>
-          <article className="country__flag">
-            <img
-              loading="lazy"
-              className="country__img"
-              src={country.flags.png}
-              alt={country.flags.alt}
-            />
-          </article>
-        </div>
-        <div className="country__details country__currencies">
-          <h2> {country.currencies.length > 1 ? "Monedas:" : "Moneda:"}</h2>
-          {country.currencies.map(currency => 
-            <p  key={currency.code}>{currency.name}({currency.symbol})</p>)
-          }
-        </div>
-        <div className="country__details country__capital">
-          <h2>Capital:</h2>
-          <p>{country.capital}</p>
-        </div>
-        <div className="country__details country__region">
-          <h2>Continente:</h2>
-          <p>{country.region}</p>
-        </div>
-        <div className="country__details country__subregion">
-          <h2>Región:</h2>
-          <p>{country.subregion}</p>
-        </div>
-        <div className="country__details country__language">
-          <h2>{languages.length>1 ? "Idiomas:" : "Idioma:"}</h2>
-          <p>{languages.join(",")}</p>
-        </div>
-        <div className="country__details country__area">
-          <h2>Área:</h2>
-          <p>{country.area.toLocaleString()} km²</p>
-        </div>
-        <div className="country__details country__population">
-          <h2>Población:</h2>
-          <p>{country.population.toLocaleString()}</p>
-        </div>
-        <div className="country__details country__timezones">
-          <h2>Husos Horarios:</h2>
-          <p>{country.timezones.join(", ")}</p>
-        </div>
-        {coatOfArms ? (
+      <div className="country-detail">
+        <section className="country">
           <div
-            className={`country__details country__coat ${
+            className={`country__details country__name ${
               !coatOfArms ? "country__name--extended" : ""
             }`}
           >
-            <h2>Escudo de Armas:</h2>
-            <img
-              loading="lazy"
-              className="country__flag"
-              src={coatOfArms}
-              alt="coat of arms"
-            />
+            <h1 className="">{name}</h1>
+            <article className="country__flag">
+              <img
+                loading="lazy"
+                className="country__img"
+                src={country.flags.png}
+                alt={country.flags.alt}
+              />
+            </article>
           </div>
-        ) : (
-          ""
-        )}
-        {/* 
-        <div className="country__details country__map">
-          <h2>Mapa</h2>
-          <div id="map" className="country__bordersFlag"></div>
-        </div> */}
-      </section>
-    </div>
-
+          <div className="country__details country__currencies">
+            <h2> {country.currencies.length > 1 ? "Monedas:" : "Moneda:"}</h2>
+            {country.currencies.map((currency) => (
+              <p key={currency.code}>
+                {currency.name}({currency.symbol})
+              </p>
+            ))}
+          </div>
+          <div className="country__details country__capital">
+            <h2>Capital:</h2>
+            <p>{country.capital}</p>
+          </div>
+          <div className="country__details country__region">
+            <h2>Continente:</h2>
+            <p>{country.region}</p>
+          </div>
+          <div className="country__details country__subregion">
+            <h2>Región:</h2>
+            <p>{country.subregion}</p>
+          </div>
+          <div className="country__details country__language">
+            <h2>{languages.length > 1 ? "Idiomas:" : "Idioma:"}</h2>
+            <p>{languages.join(",")}</p>
+          </div>
+          <div className="country__details country__area">
+            <h2>Área:</h2>
+            <p>{country.area.toLocaleString()} km²</p>
+          </div>
+          <div className="country__details country__population">
+            <h2>Población:</h2>
+            <p>{country.population.toLocaleString()}</p>
+          </div>
+          <div className="country__details country__timezones">
+            <h2>Husos Horarios:</h2>
+            <p>{country.timezones.join(", ")}</p>
+          </div>
+          {coatOfArms ? (
+            <div
+              className={`country__details country__coat ${
+                !coatOfArms ? "country__name--extended" : ""
+              }`}
+            >
+              <h2>Escudo de Armas:</h2>
+              <img
+                loading="lazy"
+                className="country__flag"
+                src={coatOfArms}
+                alt="coat of arms"
+              />
+            </div>
+          ) : (
+            ""
+          )}
+          {
+            borders && 
+            <div className="country__details country__borders">
+            <h2>Paises Limitrofes:</h2>
+            <Carousel
+              next={ (next, active) => true }
+              prev={ (prev, active) => true }
+              visibleItems={borders?.length}
+            >
+              {borders?.map(border => 
+                    <div style={{textAlign: 'center'}} key={border.cca2}> 
+                      <div>
+                      <img className="flag-border" style={{minHeight:150,maxHeight:150}} src={border.flags.png}/> 
+                      <h3>{border.name.common}</h3>
+                      </div>
+                    </div>)
+              }
+            </Carousel>
+          </div>
+          }
+        </section>
+      </div>
     </>
   );
 };
--- src/Services/CountryService.js
+++ src/Services/CountryService.js
@@ -24,9 +24,28 @@ export const getCountryByCode = async (code) => {
         if (!response.ok) {
             throw new Error("Error al obtener los datos");
         }
-        const jsonData = await response.json();
-        return jsonData.map(json => CountryJson.fromJsonToCountry(json)).pop() ;
+        const jsonData = (await response.json()).pop();
+        if(jsonData.borders){
+          const borders = await getFlagsByCodes(jsonData.borders);
+          jsonData.borders = borders
+        }
+        return CountryJson.fromJsonToCountry(jsonData) ;
     } catch (error) {
         console.error("Error:", error);
     }
+}
+
+export const getFlagsByCodes = async (codes) => {
+  try {
+    const response = await fetch(
+        `https://restcountries.com/v3.1/alpha?codes=${codes.join(",")}&fields=flags,cca2,name`
+    );
+    if (!response.ok) {
+        throw new Error("Error al obtener los datos");
+    }
+    const jsonData = await response.json();
+    return jsonData;
+  } catch (error) {
+      console.error("Error:", error);
+  }
 }
